5ba4c39d35fbf6dc2b6f6dec35162e24d6021429
Leonardo Ramírez <juanlov4321@gmail.com>
2026-01-30 11:40:39 -0500

    Refactor CreditCardValidator to improve card validation logic and update state management

--- app/components/card.checkert.tsx
+++ app/components/card.checkert.tsx
@@ -1,17 +1,20 @@
+"use client";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useState } from "react";
-import { CreditCard } from "../../interfaces/credit-card.interface";
+import { CreditCard } from "../types/credit-card";
 import { FaCcVisa, FaCcMastercard, FaCcAmex, FaCcDiscover, FaCreditCard } from "react-icons/fa";
-import { getTokenCard } from "../../services/card.service";
+
 import toast from "react-hot-toast";
-import { useSteps } from "../../stores/useTransaction";
-import { useCardToken } from "../../stores/useTransaction";
+import { useTokenCard } from "../hooks/useTokenCard";
+import { useTransactionParams } from "../stores/useTransaction";
+import { useSteps } from "../stores/useSteps";
 
 const CreditCardValidator = () => {
-	const { setStep } = useSteps();
-	const { setTokenCard } = useCardToken();
+	const { setTokenCard } = useTransactionParams();
+	const { mutateAsync: getTokenCard, isSuccess, isError, isPending } = useTokenCard();
 	const [cardType, setCardType] = useState<string | null>(null);
-	const [loading, setLoading] = useState(false);
+	const { setStep } = useSteps();
+
 	const {
 		register,
 		handleSubmit,
@@ -19,21 +22,16 @@ const CreditCardValidator = () => {
 	} = useForm<CreditCard>();
 
 	const onSubmit: SubmitHandler<CreditCard> = (data) => {
-		setLoading(true);
-		// split the card number and remove spaces
-
-		toast.promise(getTokenCard(data.number.split(" ").join(""), data.cvc, data.exp_month, data.exp_year, data.card_holder), {
+		toast.promise(getTokenCard(data), {
 			loading: "Validating card...",
-			success: (res: any) => {
-				setTokenCard(res.data.id);
-				setStep(4);
-				setLoading(false);
-				return "Card validated successfully";
+			success: (response) => {
+				if (isSuccess) {
+					setTokenCard(response?.data?.id);
+					setStep(3);
+				}
+				return "Card validated successfully!";
 			},
-			error: () => {
-				setLoading(false);
-				return "There was an error validating the card";
-			},
+			error: "Failed to validate card. Please check your details and try again.",
 		});
 	};
 
@@ -93,7 +91,7 @@ const CreditCardValidator = () => {
 									e.target.value = formatCreditCardNumber(e.target.value);
 								},
 							})}
-							disabled={loading}
+							disabled={isPending}
 							maxLength={19}
 						/>
 						<div className='absolute right-3 top-1/2 transform -translate-y-1/2'>{getCardIcon()}</div>
@@ -109,7 +107,7 @@ const CreditCardValidator = () => {
 							className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
 							placeholder='123'
 							maxLength={4}
-							disabled={loading}
+							disabled={isPending}
 							{...register("cvc", { required: true })}
 						/>
 						{errors.cvc && <span className='text-red-500 text-xs italic'>Required</span>}
@@ -122,7 +120,7 @@ const CreditCardValidator = () => {
 							className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
 							placeholder='MM'
 							maxLength={2}
-							disabled={loading}
+							disabled={isPending}
 							{...register("exp_month", { required: true })}
 						/>
 						{errors.exp_month && <span className='text-red-500 text-xs italic'>Required</span>}
@@ -135,7 +133,7 @@ const CreditCardValidator = () => {
 							className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
 							placeholder='YY'
 							maxLength={2}
-							disabled={loading}
+							disabled={isPending}
 							{...register("exp_year", { required: true })}
 						/>
 						{errors.exp_year && <span className='text-red-500 text-xs italic'>Required</span>}
@@ -148,14 +146,14 @@ const CreditCardValidator = () => {
 						type='text'
 						className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
 						placeholder='Full Name on Card'
-						disabled={loading}
+						disabled={isPending}
 						{...register("card_holder", { required: true })}
 					/>
 					{errors.card_holder && <span className='text-red-500 text-xs italic'>This field is required</span>}
 				</div>
 
-				<button type='submit' className='w-full btn btn-primary' disabled={loading}>
-					{loading ? "Validating card..." : "Validate Card"}
+				<button type='submit' className='w-full btn btn-primary' disabled={isPending}>
+					{isPending ? "Validating card..." : "Validate Card"}
 				</button>
 			</form>
 		</div>
